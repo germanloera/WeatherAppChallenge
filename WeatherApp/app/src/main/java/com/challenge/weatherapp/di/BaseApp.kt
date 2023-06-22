52080fcc3df9758536030c5e78d16a26bf1450db
german <german@DESKTOP-LN41IRB.local>
2023-06-21 19:37:01 -0500

    Update .gitignore, .gitignore, and 85 more files...

--- WeatherApp/app/src/main/java/com/challenge/weatherapp/di/BaseApp.kt
+++
@@ -1,7 +0,0 @@
-package com.challenge.weatherapp.di
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class BaseApp : Application()
---
+++ WeatherChallenge/app/src/main/java/com/challenge/weatherchallenge/di/AppModule.kt
@@ -0,0 +1,72 @@
+package com.challenge.weatherchallenge.di
+
+import android.content.Context
+import com.challenge.weatherchallenge.BuildConfig
+import com.challenge.weatherchallenge.utils.TIMEOUT_SECONDS
+import com.ihsanbal.logging.Level
+import com.ihsanbal.logging.LoggingInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun gsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Provides
+    @Singleton
+    fun getClient(): OkHttpClient {
+        val client =
+            OkHttpClient.Builder()
+                .connectTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+                .readTimeout(TIMEOUT_SECONDS, TimeUnit.SECONDS)
+
+        val interceptor = LoggingInterceptor.Builder().setLevel(Level.BODY).build()
+        client.addInterceptor(interceptor)
+
+        return client.build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun getRetrofit(client: OkHttpClient, factory: GsonConverterFactory): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(factory)
+            .client(client)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+
+    }
+
+    @Provides
+    @Singleton
+    fun getApiClient(retrofit: Retrofit): ApiClient {
+        return retrofit.create(ApiClient::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideApplication(@ApplicationContext app: Context):   BaseApp{
+        return app as BaseApp
+    }
+
+
+
+
+}
